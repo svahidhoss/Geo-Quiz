b91a49abdf216eb3205ac70a301def967df9c001
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-08-04 12:21:27 -0400

    Ch4, Moves the UI state data to Question View Model. Updates the code accordingly.

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -15,17 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     private val quizViewModel: QuizViewModel by viewModels()
 
-    private val questionList = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-
-    private var currentIndex = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
@@ -84,25 +73,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun nextQuestion() {
-        currentIndex = (currentIndex + 1) % questionList.size
+        quizViewModel.moveToNext()
         updateQuestion()
     }
 
     private fun prevQuestion() {
-        currentIndex = if (currentIndex - 1 > 0) currentIndex - 1
-        else questionList.size - 1
+        quizViewModel.moveToPrev()
         updateQuestion()
     }
 
     private fun updateQuestion() {
-        binding.questionTextView.setText(questionList[currentIndex].textResId)
+        binding.questionTextView.setText(quizViewModel.currentQuestionText)
     }
 
     /**
      * Checks the answer given by the user and updates the view
      */
     private fun checkAnswer(userAnswer: Boolean) {
-        val isAnswerCorrect = userAnswer == questionList[currentIndex].answer
+        val isAnswerCorrect = userAnswer == quizViewModel.currentQuestionAnswer
 
         val messageResId = if (isAnswerCorrect) R.string.correct_toast else R.string.incorrect_toast
 
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -7,6 +7,35 @@ private const val TAG = "QuizViewModel"
 
 class QuizViewModel : ViewModel() {
 
+    private val questionList = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
+    var hasAlreadyAnswered = false
+
+    val currentQuestionAnswer: Boolean
+        get() = questionList[currentIndex].answer
+
+
+    val currentQuestionText: Int
+        get() = questionList[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionList.size
+    }
+
+    fun moveToPrev() {
+        currentIndex = if (currentIndex - 1 > 0) currentIndex - 1
+        else questionList.size - 1
+    }
+
     init {
         Log.d(TAG, "ViewModel instance created")
     }
