40b0db43441ba34341259dbce7ecbfa5603ff07c
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-08-11 12:16:19 -0400

    Ch6: Adds two new instrumentation tests to check the text displayed upon entering the main activity. Adds one more that checks the text changes accordingly on next button press.

--- app/src/androidTest/java/com/bignerdranch/android/geoquiz/MainActivityTest.kt
+++ app/src/androidTest/java/com/bignerdranch/android/geoquiz/MainActivityTest.kt
@@ -1,10 +1,15 @@
 package com.bignerdranch.android.geoquiz
 
 import androidx.test.core.app.ActivityScenario
-import org.junit.Assert.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 
 class MainActivityTest {
 
@@ -18,5 +23,18 @@ class MainActivityTest {
     @After
     fun tearDown() {
         scenario.close()
+    }
+
+    @Test
+    fun showsFirstQuestionOnLaunch() {
+        onView(withId(R.id.question_text_view))
+            .check(matches(withText(R.string.question_australia)))
+    }
+
+    @Test
+    fun showsSecondQuestionAfterNextButtonPress() {
+        onView(withId(R.id.next_button)).perform(click())
+        onView(withId(R.id.question_text_view))
+            .check(matches(withText(R.string.question_oceans)))
     }
 }
