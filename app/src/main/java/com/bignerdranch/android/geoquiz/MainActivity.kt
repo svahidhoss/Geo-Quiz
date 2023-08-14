6abe30467b47acc09ceadffcf64011ba3cea9d35
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-08-13 20:33:50 -0400

    Ch8: Adds blurCheatButton fun for Android versions 31 and above. Challenge: adds a textview to show the api level

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.bignerdranch.android.geoquiz
 
 import android.app.Activity
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
@@ -63,6 +67,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            blurCheatButton()
+        }
+
+        binding.androidVersionTextView.text = getString(R.string.api_level_msg, Build.VERSION.SDK_INT)
     }
 
     override fun onStart() {
@@ -126,5 +136,15 @@ class MainActivity : AppCompatActivity() {
             .setAction("CLOSE") { }
             .setActionTextColor(resources.getColor(android.R.color.holo_red_light, theme))
             .show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun blurCheatButton() {
+        val effect = RenderEffect.createBlurEffect(
+            10.0f,
+            10.0f,
+            Shader.TileMode.CLAMP
+        )
+        binding.cheatButton.setRenderEffect(effect)
     }
 }
